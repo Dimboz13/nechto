4f5a2a72d55a206e20199195f9089c2860797873
Dimbo13 <dimov.dmitrii@gmail.com>
2021-03-06 19:06:39 +0200

    gitnia polnaia2

---
+++ src/lesson1/seredina.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class seredina {
+
+    public static void main(String[] args) {
+        String str = "Я все равно докопаюсь до истины";
+        for (int i = 0; i < 13; i++) {
+            System.out.println(i + " " + str);
+
+        }
+    }
+}
