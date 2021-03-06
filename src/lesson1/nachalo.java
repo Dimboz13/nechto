0c1f3370bc05212f98bb6dbafd43d53943243eb7
Dimbo13 <dimov.dmitrii@gmail.com>
2021-03-06 18:59:26 +0200

    gitnia polnaia1

---
+++ src/lesson1/nachalo.java
@@ -0,0 +1,12 @@
+package lesson1;
+
+public class nachalo {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println( i + " Привет МИР !!!");
+        }
+    }
+}
